d11f322a2ab13a05c57f03fd3e94a0a559f18633
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-03-16 10:55:56 +0000

    Added case class FrothedMilk Added case classs BeanGrindException

--- src/main/scala/Cafe.scala
+++ src/main/scala/Cafe.scala
@@ -1,5 +1,9 @@
 case class GroundCoffee(typeOfCoffee: String)
 
+case class FrothedMilk(typeOfFroth: String)
+
+case class BeanGrindException(msg: String) extends Exception
+
 object Cafe extends App {
 
   def grind(typeOfBean: String) : GroundCoffee = {
@@ -7,7 +11,8 @@ object Cafe extends App {
       case a if a == "Arabica beans" => GroundCoffee("Ground Arabica")
       case b if b == "Robusta beans" => GroundCoffee("Ground Robusta")
       case _ => throw new IllegalArgumentException("Get me some real beans...")
-    }
-  }
 
+    }
+
+  }
 }
--- src/test/scala/MilkSpec.scala
+++ src/test/scala/MilkSpec.scala
@@ -2,4 +2,8 @@ import org.scalatest.{MustMatchers, WordSpec}
 
 class MilkSpec extends WordSpec with MustMatchers {
 
+  "Milk" must {
+
+  }
+
 }
