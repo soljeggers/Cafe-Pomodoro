f9b58b1f124501ae0aca69d5380a00eaaf18dda5
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-03-16 09:39:57 +0000

    - Added Grind function - Added CoffeeBeans spec - Added Solution so beans now Grind

--- src/test/scala/CoffeeBeansSpec.scala
+++ src/test/scala/CoffeeBeansSpec.scala
@@ -4,14 +4,21 @@ class CoffeeBeansSpec extends WordSpec with MustMatchers {
 
   "CoffeeBeans" must {
 
-    "return 'Get me some real beans' when provided with anything but Arabica beans'" in {
-      CoffeeBeans.grind("Porridge") mustEqual "Get me some real beans"
+    "throw Exception with message 'Get me some real beans...' when provided with anything but 'Arabica beans'" in {
+
+      val x = intercept[IllegalArgumentException] {
+        Cafe.grind("Porridge")
+      }
+      x.getMessage mustEqual "Get me some real beans..."
     }
 
-    "return 'Ground coffee' when provided with Arabica beans" ignore  {
-      Grind("Arabica beans") mustEqual "Ground coffee"
+    "return 'Ground Arabica' when provided with 'Arabica beans' " in  {
+      Cafe.grind("Arabica beans") mustEqual GroundCoffee("Ground Arabica")
     }
 
+    "return 'Ground Robusta' when provided with 'Robusta beans'" in {
+      Cafe.grind("Robusta beans") mustEqual GroundCoffee("Ground Robusta")
+    }
 
   }
 }
