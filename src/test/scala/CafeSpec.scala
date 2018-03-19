469c24ec07f2701413ea82a9b15b884eaf6f8559
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-03-19 13:55:58 +0000

    - Added brew method - Added new Unit test - Added combine

--- src/test/scala/CafeSpec.scala
+++ src/test/scala/CafeSpec.scala
@@ -2,4 +2,12 @@ import org.scalatest.{MustMatchers, WordSpec}
 
 class CafeSpec extends WordSpec with MustMatchers {
 
+  "Cafe" must {
+
+    "return a Latte with type of milk when coffee and milk are combined together" in {
+
+      Cafe.combine(FrothedMilk("WholeMilk"), Coffee(Water(40), GroundCoffee("")) mustEqual Latte(FrothedMilk("WholeMilk"), Coffee(Water(35), GroundCoffee(""))))
+    }
+  }
+
 }
